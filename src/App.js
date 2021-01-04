09406ecef0cd2a5dd0200146d71d850e7996dcfe
HyeSuni <zaqxswcde88@naver.com>
2021-01-04 14:33:03 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-<<<<<<< HEAD
-import './App.css';
-
-import Greeting from './components/Greeting';
-import Background from './components/Background';
-import Home from './pages/Home';
-=======
 // css import
 import "./App.css";
 
 // components import
 import HomePage from "./pages/HomePage";
->>>>>>> 78ae35e84c07c98f116829ddac6faedae2a3bb11
 
 export default function App() {
   return (
     <div className="app">
-<<<<<<< HEAD
-      <Background />
-      <div className="main">
-        <Home />
-      </div>
-    </div>
-  );
-}
-=======
       <div className={"background"}>
         <div className={"background-item"}></div>
       </div>
@@ -34,4 +17,3 @@ export default function App() {
     </div>
   );
 }
->>>>>>> 78ae35e84c07c98f116829ddac6faedae2a3bb11
